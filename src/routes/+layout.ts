30c4b8c0d5d5e46fc8fcbab0569038a3aec367fc
NIYOMUGISHA Obed <obed6@duck.com>
2025-06-19 19:47:23 +0200

    Add @vercel/speed-insights integration

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,7 +4,9 @@ import { injectAnalytics } from '@vercel/analytics/sveltekit';
 import type { LayoutLoad } from './$types';
 import { entries } from 'idb-keyval';
 import { browser } from '$app/environment';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+injectSpeedInsights();
 injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 export const load: LayoutLoad = async () => ({
