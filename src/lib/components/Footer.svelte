ca75f0be346cf3222c663f3f10b608d32cdb44a5
NIYOMUGISHA Obed <obed6@duck.com>
2025-06-26 19:15:59 +0200

    Refactor footer component

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,39 +1,21 @@
 <script>
-	import { page } from '$app/state';
-
 	const year = new Date().getFullYear();
-	const { origin } = page.url;
 	const NAME = 'NIYOMUGISHA Obed';
 
 	const range = year - 2025 ? `2025 - ${year}` : year;
 </script>
 
 <footer class="container">
-	<p class="screen">
-		Copyright &copy; by {NAME}
-		{range} <br />
-		All rights reserved.
-	</p>
-
-	<p class="print">
-		Generated via
-		<a href={origin}>{origin}</a>
-	</p>
+	Copyright &copy; by {NAME}
+	{range} <br />
+	All rights reserved.
 </footer>
 
 <style>
-	p {
+	footer {
 		text-align: center;
-	}
 
-	.screen {
 		@media print {
-			display: none;
-		}
-	}
-
-	.print {
-		@media screen {
 			display: none;
 		}
 	}
