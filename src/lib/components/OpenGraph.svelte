27e57fff1bfea7685c3348b7cefedc8e9aa36f29
NIYOMUGISHA Obed <obed6@duck.com>
2025-07-21 01:18:41 +0200

    Update OpenGraph component

--- src/lib/components/OpenGraph.svelte
+++ src/lib/components/OpenGraph.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import { page } from "$app/state";
+  import { page } from '$app/state';
 
-  const TITLE = "Devis";
+  const TITLE = 'Devis';
   const DESC = `Devis (estimate in French) is a free web tool for creating
     , editing, and exporting price estimations, 
     especially for construction projects.`;
-  const url = page.url;
-  const imgURL = new URL("/og.png", url);
+  const { host, origin } = page.url;
+  const imgURL = origin + '/open-graph.png';
 </script>
 
 <svelte:head>
@@ -14,17 +14,17 @@
   <meta name="description" content={DESC} />
 
   <!-- Open Graph Meta Tags -->
-  <meta property="og:url" content={url.toString()} />
+  <meta property="og:url" content={origin} />
   <meta property="og:type" content="website" />
   <meta property="og:title" content={TITLE} />
   <meta property="og:description" content={DESC} />
-  <meta property="og:image" content={imgURL.toString()} />
+  <meta property="og:image" content={imgURL} />
 
   <!-- Twitter Meta Tags -->
   <meta name="twitter:card" content="summary_large_image" />
-  <meta property="twitter:domain" content={url.hostname} />
-  <meta property="twitter:url" content={url.toString()} />
+  <meta property="twitter:domain" content={host} />
+  <meta property="twitter:url" content={origin} />
   <meta name="twitter:title" content={TITLE} />
   <meta name="twitter:description" content={DESC} />
-  <meta name="twitter:image" content={imgURL.toString()} />
+  <meta name="twitter:image" content={imgURL} />
 </svelte:head>
